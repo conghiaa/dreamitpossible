7aa29f68326c4d09cad2056f9cf6a5078e55ed8e
conghiaa <conghiaa@outlook.com>
2018-11-05 13:46:15 +0700

    Add article sys page, related js, styles

--- scripts/index.js
+++ scripts/index.js
@@ -15,3 +15,26 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// markdown to html ajax
+function markdownToHtml(){
+  let targetURL = 'https://api.github.com/markdown';
+  let markdown = $('#content_md').val();
+  let data = {
+    "text": markdown,
+    "mode": "gfm",
+  };
+  $.ajax({url: targetURL,
+          method: 'POST',
+          contentType: 'application/json',
+          dataType: 'html',
+          data:JSON.stringify(data),
+  }).done(function(data){
+    $('#content_html').val(data);
+    $('#html-box').html(data);
+  })
+}
+
+$(document).ready(() => {
+  $('#preview-btn').click(markdownToHtml);
+});
